2c717977d93be529d9aa7f78e50b9b532ffdaa59
Your Name <kalaitzidispaul@gmail.com>
2022-05-12 08:25:00 -0400

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import Logo from "/Users/paulkalait/Desktop/webdeveloper/projects/stock-app/src/robinhood.svg";
+import '../Header.css'
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header__wrapper">
+      {/* logo */}
+      <div className="header__logo">
+        <img src={Logo} width={25} />
+      </div>
+      {/* search */}
+      <div className="header__search">
+        <div className="header__searchContainer">
+          <input placeholder="Search" type='text' />
+        </div>
+      </div>
+      {/* menu */}
+      <div className="header__menuItems">
+      <a href='#'>Free Stocks</a>
+      <a href='#'>Portfolio</a>
+      <a href='#'>Cash</a>
+      <a href='#'>Messages</a>
+      <a href='#'>Account</a>
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
