926bf0dd57aee51014ad8c8c2524953c6da09030
Your Name <kalaitzidispaul@gmail.com>
2022-05-11 21:21:11 -0400

    added header component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      <div className='app__header'>
+      <Header />
+      </div>
+      {/* body */}
+      <div className='app__body'>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
